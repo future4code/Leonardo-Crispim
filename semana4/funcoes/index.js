54faa7acd052fbc3f7292aa1b5e0b93c508c59d3
Leonardo Crispim <leonardo.crispim@outlook.com.br>
2020-02-03 17:33:54 -0200

    exercicios incompletos, dia dificil

---
+++ semana4/funcoes/index.js
@@ -0,0 +1,35 @@
+/*
+
+1 -
+
+A) O resultado sera um array vazio
+
+B) ??
+
+C) ???
+
+--
+
+2 -
+
+A) 0, 2, undefined
+
+B) Apenas se o numero que fosse printado no console fosse igual a um dos numeros do array
+
+--
+
+3) ?
+
+--
+
+4 -
+
+A)
+
+
+
+B) 
+
+
+*/
+
