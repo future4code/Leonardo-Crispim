b516b806659d1cc95a2d8f35110033dee07315a5
Leonardo Crispim <leonardo.crispim@outlook.com.br>
2020-03-06 16:36:25 -0300

    Faltou luz aqui, primeiro commit para entrega obrigatoria, vou continuar o projeto sozinho

---
+++ semana7/aula4-spotif4/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
+
+const MainDiv = styled.div`
+`
+
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state ={
+    }
+  }
+
+  render(){
+    return(
+      <MainDiv>
+      </MainDiv>
+    )
+  }
+
+}
+
+export default App;
