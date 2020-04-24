3ef45c936489a9bb3fcfc77eeab985d66ec9168f
Leonardo Crispim <leonardo.crispim@outlook.com.br>
2020-04-24 17:31:11 -0300

    projeto semanal atrasado, iniciado

---
+++ semana14/projetosemanal/build/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const operationType = process.argv[2];
+const name = process.argv[3];
+const cpf = process.argv[4];
+const data = process.argv[5];
+console.log(operationType, name, cpf, data);
+//# sourceMappingURL=index.js.map
